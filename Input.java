4008e8526192b0d3d498eb0e1045d08ea202594f
Shavali388 <dudekulas54@gmail.com>
2025-07-16 13:01:47 +0530

    Updated

--- Input.java
+++ Input.java
@@ -3,5 +3,8 @@ classs Input
   public static void main(String[] args)
     {
     System.out.println("Enter some data");
+    System.out.println("Enter Name");
+    System.out.println("Enter Date");
+    
   }
 }
